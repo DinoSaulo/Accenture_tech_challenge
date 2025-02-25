494988d0d9e7620a10b8d592c4dbc8f769604ddc
Saulo Barros <saulobarros1997@gmail.com>
2025-02-25 11:34:18 -0300

    feat: implementando testes com os livros com o gherkin

--- cypress/e2e/demoqa/demoqa.feature
+++ cypress/e2e/demoqa/demoqa.feature
@@ -16,3 +16,22 @@ Feature: Gerenciamento de Usuário e Livros na API DemoQA
     When eu envio uma solicitação para gerar um token de acesso com as credenciais do usuário
     Then o status da resposta deve ser 200
     And o corpo da resposta deve indicar que o usuário está autorizado
+
+  Scenario: Listar todos os livros disponíveis
+    Given que a API está disponível
+    When eu envio uma solicitação para listar os livros disponíveis
+    Then o status da resposta deve ser 200
+    And o corpo da resposta deve conter uma lista de livros
+
+  Scenario: Alugar dois livros disponíveis
+    Given que o usuário está logado e tem um token de acesso válido
+    And que há livros disponíveis na biblioteca
+    When eu envio uma solicitação para alugar dois livros específicos
+    Then o status da resposta deve ser 201
+    And o corpo da resposta deve indicar que os livros foram alugados com sucesso
+
+  Scenario: Listar os detalhes do usuário com os livros escolhidos
+    Given que o usuário alugou dois livros
+    When eu envio uma solicitação para obter os detalhes do usuário
+    Then o status da resposta deve ser 200
+    And o corpo da resposta deve conter as informações do usuário junto com os livros alugados
--- cypress/support/step_definitions/ApiTestSteps.js
+++ cypress/support/step_definitions/ApiTestSteps.js
@@ -1,12 +1,12 @@
 
 import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
-import messages from '../../fixtures/messages.json'
 import jwt from 'jsonwebtoken';
 
 /* global Given, Then, When */
 let requestResponse = {}
 let userId;
 let token;
+let books;
 let username = `${Cypress.env('username')}`
 
 Given("que eu envio uma solicitação para criar um usuário com um nome e senha válidos", () => {
@@ -40,9 +40,7 @@ When("eu envio uma solicitação para gerar um token de acesso com as credenciai
       userName: temp_user,
       password: `${Cypress.env('password')}`
     }
-  }).then((response) => {
-    //requestResponse = response
-  });
+  })
 
   cy.request({
     method: 'POST',
@@ -73,5 +71,138 @@ Then("o corpo da resposta deve indicar que o usuário está autorizado", () => {
     expect(requestResponse.body.result).to.be.equal(messages.authorized_msg);
     expect(requestResponse.body.status).to.be.equal(messages.success_title_msg);
   })
+})
+
+Given("que a API está disponível", () => {})
+
+When("eu envio uma solicitação para listar os livros disponíveis", () => {
+  cy.request({
+    method: 'GET',
+    url: `${Cypress.config('baseUrl')}/BookStore/v1/Books`,
+  }).then((response) => {
+    requestResponse = response
+  });
+})
+
+Then("o corpo da resposta deve conter uma lista de livros", () => {
+  expect(requestResponse.body.books).to.be.an('array').that.is.not.empty;
+})
+
+Given("que o usuário está logado e tem um token de acesso válido", () => {
+  const temp_user = username.replace("DATE", Date.now())
+  cy.request({
+    method: 'POST',
+    url: `${Cypress.config('baseUrl')}/Account/v1/User`,
+    body: {
+      userName: temp_user,
+      password: `${Cypress.env('password')}`
+    }
+  }).then((response) => {
+    userId = response.body.userID
+    cy.request({
+      method: 'POST',
+      url: `${Cypress.config('baseUrl')}/Account/v1/GenerateToken`,
+      body: {
+        userName: temp_user,
+        password: `${Cypress.env('password')}`
+      }
+    }).then((response) => {
+      token = response.body.token
+    });
+  });
+})
+
+Given("que há livros disponíveis na biblioteca", () => {
+  cy.request({
+    method: 'GET',
+    url: `${Cypress.config('baseUrl')}/BookStore/v1/Books`,
+  }).then((response) => {
+    expect(response.body.books).to.be.an('array').that.is.not.empty;
+    books = response.body.books
+  });
+})
+
+Given("eu envio uma solicitação para alugar dois livros específicos", () => {
+  const book1 = books[0].isbn;
+  const book2 = books[1].isbn;
   
+  cy.request({
+    method: 'POST',
+    url: `${Cypress.config().baseUrl}/BookStore/v1/Books`,
+    headers: { Authorization: `Bearer ${token}` },
+    body: {
+      userId: userId,
+      collectionOfIsbns: [{ isbn: book1 }, { isbn: book2 }]
+    }
+  }).then((request) => {
+    requestResponse = request
+  });
+
+})
+
+Then("o corpo da resposta deve indicar que os livros foram alugados com sucesso", () => {
+  expect(requestResponse.body.books).to.be.an('array').that.is.not.empty;
+  expect(requestResponse.body.books).to.have.length(2);
+  expect(requestResponse.body.books[0].isbn).to.be.equals(books[0].isbn);
+  expect(requestResponse.body.books[1].isbn).to.be.equals(books[1].isbn);
+})
+
+Given("que o usuário alugou dois livros", () => {
+  cy.request({
+    method: 'GET',
+    url: `${Cypress.config('baseUrl')}/BookStore/v1/Books`,
+  }).then((response) => {
+    expect(response.body.books).to.be.an('array').that.is.not.empty;
+    books = response.body.books
+  });
+
+  const temp_user = username.replace("DATE", Date.now())
+  cy.request({
+    method: 'POST',
+    url: `${Cypress.config('baseUrl')}/Account/v1/User`,
+    body: {
+      userName: temp_user,
+      password: `${Cypress.env('password')}`
+    }
+  }).then((response) => {
+    userId = response.body.userID
+    cy.request({
+      method: 'POST',
+      url: `${Cypress.config('baseUrl')}/Account/v1/GenerateToken`,
+      body: {
+        userName: temp_user,
+        password: `${Cypress.env('password')}`
+      }
+    }).then((response) => {
+      cy.request({
+        method: 'POST',
+        url: `${Cypress.config().baseUrl}/BookStore/v1/Books`,
+        headers: { Authorization: `Bearer ${response.body.token}` },
+        body: {
+          userId: userId,
+          collectionOfIsbns: [{ isbn: books[0].isbn }, { isbn: books[1].isbn }]
+        }
+      }).then((request) => {
+        requestResponse = request
+        token = response.body.token
+      });
+    });
+  });
+})
+
+When("eu envio uma solicitação para obter os detalhes do usuário", () => {
+  cy.request({
+    method: 'GET',
+    url: `${Cypress.config().baseUrl}/Account/v1/User/${userId}`,
+    headers: { Authorization: `Bearer ${token}` }
+  }).then((response) => {
+    requestResponse = response
+  });
+})
+
+Then("o corpo da resposta deve conter as informações do usuário junto com os livros alugados", () => {
+  expect(requestResponse.body.books).to.be.an('array').that.is.not.empty;
+  expect(requestResponse.body.books).to.have.length(2);
+  expect(requestResponse.body.books[0].isbn).to.be.equals(books[0].isbn);
+  expect(requestResponse.body.books[1].isbn).to.be.equals(books[1].isbn);
 })
